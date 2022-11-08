0dc923f03fff0e81b6e3084c41c82c6c1e8636dd
MarcosGutierrezMoya <marcos.gutierrez.moya2010@gmail.com>
2022-11-08 16:05:57 +0100

    Make the closing windows in the FAQ section

---
+++ assets/js/cesta.js
@@ -0,0 +1,1 @@
+cantidadProducto.innerText = 1;
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,56 @@
-let flechas = [];
-flechas = document.getElementsByClassName("flechaFaq");
-for (let i = 0; i < 2; i++) {
-    flechas.addEventListener("click",occult);
+// let flechas = [];
+// flechas = document.getElementsByClassName("flechaFaq");
+for (let i = 0; i < document.getElementsByClassName("flechaFaq").length; i++) {
+    document.getElementsByClassName("flechaFaq")[i].addEventListener("click",occult);
 }
+for (let i = 0; i < document.getElementsByClassName("masProducto").length; i++) {
+    document.getElementsByClassName("masProducto")[i].addEventListener("click",añadir);
+    console.log("hola");
+}
+document.getElementById("text2").hidden = true;
 
 function occult(i) {
     let p1 = document.getElementById("text0");
-    console.log(p1);
-    switch (i) {
-        case i.id=="flecha1":
-            (p1.hidden==true)?p1.hidden=false:p1.hidden=true;
+    let p2 = document.getElementById("text1");
+    let p3 = document.getElementById("text2");
+    let srcUp ="assets/img/flechitaArriba.svg";
+    let srcDown ="assets/img/flechitaAbajo.svg";
+
+    switch (i.target.id) {
+        case "flecha1":
+            console.log(p1);
+            if(p1.hidden==true){
+                p1.hidden=false;
+                this.src=srcUp;
+            } else{
+                p1.hidden=true;
+                this.src=srcDown;
+            }
             break;
-        case 1:
-            (document.getElementById("text1").hidden==true)?document.getElementById("text1").hidden=false:document.getElementById("text1").hidden=true;
-        break;
-        case 2:
-            (document.getElementById("text2").hidden==true)?document.getElementById("text2").hidden=false:document.getElementById("text2").hidden=true;
+        case "flecha2":
+            if(p2.hidden==true){
+                p2.hidden=false;
+                this.src=srcUp;
+            } else{
+                p2.hidden=true;
+                this.src=srcDown;
+            }
+            break;
+        case "flecha3":
+            if(p3.hidden==true){
+                p3.hidden=false;
+                this.src=srcUp;
+            } else{
+                p3.hidden=true;
+                this.src=srcDown;
+            }
             break;
         default:
             break;
     }
+}
+
+function añadir() {
+    console.log(this.previousSibling.lastChild);
+    // document.previousSibling.lastChild
 }
